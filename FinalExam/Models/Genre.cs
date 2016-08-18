06be50f73fcc773c25e5a65888559b91e6965636
KarenSpringford <jkspringford@gmail.com>
2016-08-18 10:41:21 -0400

    Final Exam

---
+++ FinalExam/Models/Genre.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalExam.Models
+{
+    public class Genre
+    {
+        /// <summary>
+        /// This is the empty constructor
+        /// </summary>
+        /// 
+        public Genre()
+        {
+
+        }
+
+        /// <summary>
+        /// This constructor takes one parameter - Name
+        /// </summary>
+        /// 
+        public Genre (string Name)
+        {
+            this.Name = Name;
+        }
+
+        public int GenreID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ FinalExam/Models/Song.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalExam.Models
+{
+    public class Song
+    {
+        public int SongID { get; set; }
+
+        public string Title { get; set; }
+
+        public int Duration { get; set; }
+    }
+}
